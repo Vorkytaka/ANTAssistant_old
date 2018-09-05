7cf8d5dd4143075828b9579b9f8a867ebb694e5d
Konstantin Dovnar <solidlsnake@yandex.ru>
2018-09-06 01:18:37 +0300

    • Подключение Koin • Класс Application

---
+++ app/src/main/java/com/assistant/ant/solidlsnake/antassistant/presentation/App.kt
@@ -0,0 +1,10 @@
+package com.assistant.ant.solidlsnake.antassistant.presentation
+
+import android.app.Application
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // todo: Инициализация Koin
+    }
+}
