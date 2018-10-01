2e273372a0afc9243d063b689a465f8404be2f56
Konstantin Dovnar <solidlsnake@yandex.ru>
2018-10-01 22:26:01 +0300

    • Подключение Kotpref • Исправление версии корутин

--- app/src/main/java/com/assistant/ant/solidlsnake/antassistant/presentation/App.kt
+++ app/src/main/java/com/assistant/ant/solidlsnake/antassistant/presentation/App.kt
@@ -1,10 +1,12 @@
 package com.assistant.ant.solidlsnake.antassistant.presentation
 
 import android.app.Application
+import com.chibatching.kotpref.Kotpref
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        Kotpref.init(this)
         // todo: Инициализация Koin
     }
 }
