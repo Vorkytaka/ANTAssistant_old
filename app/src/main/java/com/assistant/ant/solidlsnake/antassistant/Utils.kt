c16425ebb359e6f3889a09ef2a6354ee16a06f44
Konstantin Dovnar <solidlsnake@yandex.ru>
2018-10-13 16:16:15 +0300

    • TextView.textChanges на корутинах

--- app/src/main/java/com/assistant/ant/solidlsnake/antassistant/Utils.kt
+++ app/src/main/java/com/assistant/ant/solidlsnake/antassistant/Utils.kt
@@ -1,3 +1,36 @@
 package com.assistant.ant.solidlsnake.antassistant
 
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.TextView
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
+
 fun CharSequence?.orEmpty(): String = this?.toString() ?: ""
+
+/**
+ * Возвращает канал, который реагирует на изменения в TextView.
+ */
+fun TextView.textChanges(capacity: Int = 1): ReceiveChannel<CharSequence> {
+    val channel = Channel<CharSequence>(capacity)
+
+    val listener = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {}
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            if (!channel.isClosedForSend) {
+                channel.offer(s ?: "")
+            }
+        }
+    }
+
+    this.addTextChangedListener(listener)
+
+    channel.invokeOnClose {
+        this.removeTextChangedListener(listener)
+    }
+
+    return channel
+}
