09abf063423bfa7153a49f7d98a9b03369c4dcae
Vorkytaka <solidlsnake@yandex.ru>
2018-12-19 23:42:49 +0300

    • Более sexy проверка параметров в Auth

--- app/src/main/java/com/assistant/ant/solidlsnake/antassistant/domain/interactor/Auth.kt
+++ app/src/main/java/com/assistant/ant/solidlsnake/antassistant/domain/interactor/Auth.kt
@@ -3,7 +3,6 @@ package com.assistant.ant.solidlsnake.antassistant.domain.interactor
 import com.assistant.ant.solidlsnake.antassistant.domain.entity.Credentials
 import com.assistant.ant.solidlsnake.antassistant.domain.repository.IRepository
 import com.assistant.ant.solidlsnake.antassistant.domain.state.AuthState
-import kotlinx.coroutines.channels.consumeEach
 
 /**
  * Авторизация пользователя
@@ -12,12 +11,9 @@ class Auth(
         private val repository: IRepository
 ) : UseCase<Credentials, AuthState>() {
     override suspend fun execute(action: (AuthState) -> Unit) {
-        if (params == null) {
-            throw IllegalStateException()
-        }
+        val p = params ?: throw IllegalStateException()
 
-        repository.auth(params!!).consumeEach {
-            action(if (it) AuthState.Success else AuthState.Error)
-        }
+        val auth = repository.auth(p).receive()
+        action(if (auth) AuthState.Success else AuthState.Error)
     }
 }
