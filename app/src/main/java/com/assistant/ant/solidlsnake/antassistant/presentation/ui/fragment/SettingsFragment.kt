48a1c3fc5ee865e417896e7ca1c7212308403892
Vorkytaka <solidlsnake@yandex.ru>
2019-05-21 01:27:19 +0300

    • Кнопка выхода на экране настроек

--- app/src/main/java/com/assistant/ant/solidlsnake/antassistant/presentation/ui/fragment/SettingsFragment.kt
+++ app/src/main/java/com/assistant/ant/solidlsnake/antassistant/presentation/ui/fragment/SettingsFragment.kt
@@ -1,5 +1,12 @@
 package com.assistant.ant.solidlsnake.antassistant.presentation.ui.fragment
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.assistant.ant.solidlsnake.antassistant.R
 
-class SettingsFragment : Fragment()
+class SettingsFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? = inflater.inflate(R.layout.fragment_settings, container, false)
+}
