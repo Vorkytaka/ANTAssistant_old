bb094d925011f612c5076edb8e72c1745760f3de
Vorkytaka <solidlsnake@yandex.ru>
2019-05-24 01:08:51 +0300

    • Чуть переделан Logout

--- app/src/main/java/com/assistant/ant/solidlsnake/antassistant/presentation/ui/fragment/SettingsFragment.kt
+++ app/src/main/java/com/assistant/ant/solidlsnake/antassistant/presentation/ui/fragment/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.assistant.ant.solidlsnake.antassistant.presentation.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.transition.TransitionManager
 import android.support.v4.app.Fragment
@@ -9,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.assistant.ant.solidlsnake.antassistant.R
 import com.assistant.ant.solidlsnake.antassistant.presentation.presenter.SettingsPresenter
+import com.assistant.ant.solidlsnake.antassistant.presentation.ui.activity.LaunchActivity
 import com.assistant.ant.solidlsnake.antassistant.presentation.view.SettingsView
 import kotlinx.android.synthetic.main.fragment_settings.*
 import org.koin.android.ext.android.inject
@@ -47,6 +49,9 @@ class SettingsFragment : Fragment(), SettingsView {
     }
 
     override fun logout() {
+        val intent = Intent(requireActivity(), LaunchActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        requireActivity().startActivity(intent)
         requireActivity().finish()
     }
 }
